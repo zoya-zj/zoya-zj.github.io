136573850d9ed520662a73272f22f41c4d2e5caf
zoya_zj <zoyay.zj@gmail.com>
2024-10-22 10:48:15 -0400

    semicolons

--- js/color_change.js
+++ js/color_change.js
@@ -3,13 +3,13 @@ let button_img = document.getElementById("button-img-id");
 
 function changeColor() {
     if (div.style.backgroundColor === "black"){
-        div.style.backgroundColor = "#f6bacb"
-        div.style.borderColor = "#f6bacb"
-        button_img.src = "img/light.png"
+        div.style.backgroundColor = "#f6bacb";
+        div.style.borderColor = "#f6bacb";
+        button_img.src = "img/light.png";
     }
     else {
-        div.style.backgroundColor = "black"
-        div.style.borderColor = "black"
-        button_img.src = "img/dark.png"
+        div.style.backgroundColor = "black";
+        div.style.borderColor = "black";
+        button_img.src = "img/dark.png";
     }
 }
