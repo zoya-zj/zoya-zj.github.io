4d0dc3fd02e8da9ee2911e6408172d408e95b20f
zoya_zj <zoyay.zj@gmail.com>
2024-10-22 10:43:25 -0400

    fixed border size change

--- js/color_change.js
+++ js/color_change.js
@@ -4,12 +4,12 @@ let button_img = document.getElementById("button-img-id");
 function changeColor() {
     if (div.style.backgroundColor === "black"){
         div.style.backgroundColor = "#f6bacb"
-        div.style.border = "#f6bacb"
+        div.style.borderColor = "#f6bacb"
         button_img.src = "img/light.png"
     }
     else {
         div.style.backgroundColor = "black"
-        div.style.border = "black"
+        div.style.borderColor = "black"
         button_img.src = "img/dark.png"
     }
 }
